89fc382d64bc8bb796935cd47f1156f35b1c317a
Björn Andersson <lord_bear@hotmail.com>
2021-01-22 12:04:01 +0100

    Cinema tickets

--- Program.cs
+++ Program.cs
@@ -8,57 +8,83 @@ namespace Övn2FlowControl
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Välkommen till hvudmenyn!");
-            Console.WriteLine("Härifrån kommer du att använda siffror för att navigera vidare och testa olika funktioner ");
-            Console.WriteLine("Menyval 0) Avsluta program ");
-            Console.WriteLine("Menyval 1) Bio biljetter");
-            Console.WriteLine("Menyval 2) Upprepa tio gånger");
-            Console.WriteLine("Menyval 3) Det tredje ordet");
-            string result = Console.ReadLine();
-
-        }
-        static string GetChoise (int ChoiseNum)
-        { 
-            string ChoiseName;
-
-            switch (ChoiseNum)
+            int num = 0;
+            do 
             {
-                case 0:
-                    ChoiseName = "Exit";
-                    break;
 
-                case 1:
-                    ChoiseName = "bio biljetter";
-                    break;
+                Console.WriteLine("Welcome to the main menu!");
+                Console.WriteLine("From here you will use numbers to navigate further and test different functions ");
+                Console.WriteLine("Option 0) Exit program ");
+                Console.WriteLine("Option 1) Cinema tickets 1 person");
+                Console.WriteLine("Option 2) Cinema tickets more than one person");
+                Console.WriteLine("Option 3) Repeat ten times");
+                Console.WriteLine("Option 4) The third word");
 
-                case 2:
-                    ChoiseName = "Upprepa tio gånger";
-                    break;
+                switch (Console.ReadLine())
+                {
+                    case "0":
+                        Console.WriteLine("You will now exit the program");
 
-                case 3:
-                    ChoiseName = "Det tredje ordet";
-                    break;
+                        break;
 
+                    case "1":
+                        Console.WriteLine("Welcome to the Cinema!");
+                        Console.WriteLine("How old are you?");
+                        string Ticket = Console.ReadLine();
+                        int ticket = Int32.Parse(Ticket);
+                        if (ticket >= 60)
+                        {
+                            Console.WriteLine("You will pay senior price of 90 kr");
+                        }
+                        else if (ticket <= 20)
+                        {
+                            Console.WriteLine("You will pay junior price of 80 kr");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You will pay adult price of 120 kr");
+                        }
+                        break;
 
-                default:
-                    ChoiseName = "Felaktig input";
-                    break;
+                      case "2":
+                        Console.WriteLine("Welcome to the Cinema!");
+                        Console.WriteLine("How many tickets do you want?");
+                        string ticketNum = Console.ReadLine();
+                        int TicketNum = Int32.Parse(ticketNum);
+                        
+                        for (int x  = 0; x < TicketNum; x++)
+                        {
+                            Console.WriteLine("How old is the person");
+                            Console.ReadLine();
+                            if (TicketNum >= 60)
+                            {
+                                Console.WriteLine("You will pay senior price of 90 kr for this person");
+                            }
+                            else if (TicketNum <= 20)
+                            {
+                                Console.WriteLine("You will pay junior price of 80 kr for this person");
+                            }
+                            else
+                            {
+                                Console.WriteLine("You will pay adult price of 120 kr for this person");
+                            }
+                         
+                        }
 
+                        break;
+                        
 
-            }
-                return ChoiseName;
+                           
+                    case "3":
+                        break;
+                }
+
+            } while (num != 0);
         }
     }
 }
 
-             /*int num01 = 0;
-             while (num01 < 10)
-             {
-                 Console.WriteLine("loop nr " + num01);
-                 num01++;
-             }
-            */
 
-//Console.WriteLine(GetChoise(0));
